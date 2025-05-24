e6bf14998a8754fa4c82f9af77f92b831bf7e4b6
octaviobottari <octaviobottari7@gmail.com>
2025-05-24 19:55:39 -0300

    Assignment 3

--- database/index.js
+++ database/index.js
@@ -1,22 +1,19 @@
 const { Pool } = require("pg")
 require("dotenv").config()
+
 /* ***************
  * Connection Pool
- * SSL Object needed for local testing of app
- * But will cause problems in production environment
- * If - else will make determination which to use
+ * Configured for both development and production
+ * Uses SSL with rejectUnauthorized: false for Render.com compatibility
+ * Exports a query method for compatibility with inventory-model.js
  * *************** */
-let pool
-if (process.env.NODE_ENV == "development") {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 })
 
-// Added for troubleshooting queries
-// during development
 module.exports = {
   async query(text, params) {
     try {
@@ -28,10 +25,4 @@ module.exports = {
       throw error
     }
   },
-}
-} else {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  })
-  module.exports = pool
 }
